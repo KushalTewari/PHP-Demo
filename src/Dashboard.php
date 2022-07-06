dc8468b94bebaea4999308f32ab430b09f41e674
Kushal Tewari <76678449+KushalTewari@users.noreply.github.com>
2022-07-06 20:50:56 +0530

    Initial Commit

--- src/Dashboard.php
+++ src/Dashboard.php
@@ -19,9 +19,10 @@
     <title>Document</title>
 </head>
 <body>
-    <table id='result'></table>
+    <table id='result' style="margin-bottom: 3%"></table>
 
-    <label>Search a user: <input type="text" id="search" /></label>
+    <label>Search an email: <input type="text" id="search" /></label>
+    <p>Suggestions: <span id="suggestions"></span></p>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="script.js"></script>
 </body>
